6284c0315fe04f2028ff48cf2d069ad295735fe8
Artem Rossokha <arossokha@takeforce.com>
2015-10-19 14:23:54 +0000

    Add console command for cleanup jobs

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ http://intelligentbee.com/blog/2013/08/07/symfony2-jobeet-day-1-starting-up-the-
 to generate code covearge docs you need tou use php unit command
 `phpunit --coverage-html=code_docs/ -c app/`
 code_docs folder added to git ignore
+
+create command for cleanup jobs
+configure cron with 
+* 1 * * * cd [project_path] && php app/console ibw:jobeet:cleanup
---
+++ src/Art/JobtestBundle/Command/JobtestCleanupCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Art\JobtestBundle\Command;
+ 
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Art\JobtestBundle\Entity\Job;
+ 
+class JobtestCleanupCommand extends ContainerAwareCommand {
+ 
+  protected function configure()
+  {
+      $this
+          ->setName('art:jobtest:cleanup')
+          ->setDescription('Cleanup Jobtest database')
+          ->addArgument('days', InputArgument::OPTIONAL, 'The email', 90)
+    ;
+  }
+ 
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+      $days = $input->getArgument('days');
+ 
+      $em = $this->getContainer()->get('doctrine')->getManager();
+      $nb = $em->getRepository('ArtJobtestBundle:Job')->cleanup($days);
+ 
+      $output->writeln(sprintf('Removed %d stale jobs', $nb));
+  }
+}
